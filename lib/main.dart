3d1a31f3d04e8d57641d5fff26beafe6adbf2822
Mamin-Andy <pg.mohammadamin@gmail.com>
2025-02-20 18:54:05 +0330

    finish app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main(List<String> args) {
   runApp(Applications());
@@ -10,6 +11,7 @@ class Applications extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'Vazir'),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: _getAppBar(),
@@ -24,16 +26,23 @@ class Applications extends StatelessWidget {
   Widget _getContents() {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 11),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(
-            width: double.infinity,
-            height: 15,
-          ),
-          _getHeader(),
-          SizedBox(height: 15),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: 15,
+            ),
+            _getHeader(),
+            SizedBox(height: 15),
+            _getSocialMedia(),
+            SizedBox(height: 15),
+            _getSkills(),
+            SizedBox(height: 20),
+            _getResume(),
+          ],
+        ),
       ),
     );
   }
@@ -49,7 +58,7 @@ class Applications extends StatelessWidget {
         Text(
           'Mohammadamin Amirkolaei Andy\nMobile Delveloper by Flutter',
           style: TextStyle(
-            fontFamily: 'Vazir',
+            // fontFamily: 'Vazir',
             fontWeight: FontWeight.bold,
             fontSize: 17,
           ),
@@ -59,7 +68,7 @@ class Applications extends StatelessWidget {
         Text(
           'I am a Flutter developer with one year of experience in designing and implementing cross-platform applications. My focus is on creating smooth, engaging, and optimized user experiences. With a creative and problem-solving mindset, I am always eager to learn new technologies and enhance my skills. I am excited to join your team, learn from experienced members, and collaborate to build high-quality and innovative applications together.',
           style: TextStyle(
-            fontFamily: 'Vazir',
+            // fontFamily: 'Vazir',
             fontWeight: FontWeight.bold,
             fontSize: 12,
           ),
@@ -76,12 +85,91 @@ class Applications extends StatelessWidget {
       title: Text(
         'MohammadAmin - Andy',
         style: TextStyle(
-          fontFamily: 'Vazir',
+          // fontFamily: 'Vazir',
           fontWeight: FontWeight.bold,
           fontSize: 22,
         ),
       ),
       centerTitle: true,
+    );
+  }
+
+  Widget _getSocialMedia() {
+    return Wrap(
+      spacing: 15,
+      children: [
+        Icon(
+          FontAwesomeIcons.github,
+          color: Colors.black,
+          size: 30,
+        ),
+        Icon(
+          FontAwesomeIcons.linkedin,
+          color: Colors.blue,
+          size: 30,
+        ),
+        Icon(
+          Icons.mail,
+          color: Colors.red,
+          size: 30,
+        ),
+      ],
+    );
+  }
+
+  Widget _getSkills() {
+    var list = ['Dart', 'Flutter', 'Git'];
+    return Wrap(
+      children: [
+        for (var skill in list)
+          Card(
+            shadowColor: Colors.blue,
+            elevation: 10,
+            child: Container(
+              padding: EdgeInsets.all(2),
+              width: 80,
+              child: Column(
+                children: [
+                  Image.asset('images/$skill.png'),
+                  Text('$skill'),
+                ],
+              ),
+            ),
+          ),
+      ],
+    );
+  }
+
+  _getResume() {
+    var list = [
+      'Computer Engineering student at Islamic Azad University, North Tehran Branch, since September 2022.'
+    ];
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+      color: Colors.grey[500],
+      child: Column(
+        children: [
+          Text(
+            'My Resume:',
+            style: TextStyle(
+              // fontFamily: 'vazir',
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(height: 5),
+          for (var resume in list)
+            Text(
+              '$resume',
+              style: TextStyle(
+                // fontFamily: 'vazir',
+                fontWeight: FontWeight.w700,
+                fontSize: 12,
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
